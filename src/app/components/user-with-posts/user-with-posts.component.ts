a770c7d762f7e582c3adab7e78655c45ef61aed1
hyrycham@gmail.com <hyrycham@gmail.com>
2021-07-28 00:29:20 +0300

    lesson2 add users-with-posts3

---
+++ src/app/components/user-with-posts/user-with-posts.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IUser} from "../../models/IUser";
+import {IPost} from "../../models/IPost";
+import {UserService} from "../../services/user.service";
+
+@Component({
+  selector: 'app-user-with-posts',
+  templateUrl: './user-with-posts.component.html',
+  styleUrls: ['./user-with-posts.component.scss']
+})
+export class UserWithPostsComponent implements OnInit {
+  @Input()
+  user: IUser;
+
+  posts:IPost[];
+
+  constructor(private userService:UserService ) { }
+
+  ngOnInit(): void {
+      this.userService.getPostByUserId(this.user.id).subscribe(value=> {this.posts=value});
+ console.log(this.user.id)
+  }
+}
