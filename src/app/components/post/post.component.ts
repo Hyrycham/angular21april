bf113cb938b7974177f92f904065396a343346b6
hyrycham@gmail.com <hyrycham@gmail.com>
2021-08-05 20:53:53 +0300

    add  post button

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,18 +1,21 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {IPost} from "../../models/iPost";
-import {ActivatedRoute, Router} from "@angular/router";
+// import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
+
 export class PostComponent implements OnInit {
   @Input()
   post: IPost;
+   show=true;
   constructor(
     private  router:Router,
-    private activatedRoute: ActivatedRoute,
+    // private activatedRoute: ActivatedRoute,
 
   ) { }
 
@@ -20,6 +23,9 @@ export class PostComponent implements OnInit {
   }
   navigateToPostDetails(): void{
     // this.router.navigate([this.post.id],{relativeTo:this.activatedRoute,state:this.post})
-    this.router.navigate(['posts/'+this.post.id],{state:this.post})
-  };
+    this.show=false;
+    this.router.navigate(['posts/'+this.post.id],{state:this.post});
+       };
+
+
 }
